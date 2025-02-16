afb48943f4a40a0f64800f2bfc3f462120da1789
gravitino <ngobinh0312@gmail.com>
2025-02-16 16:09:32 +0700

    add public path

--- src/main/java/charitan_go/charitan_api_gateway/internal/filter/JwtAuthFilter.java
+++ src/main/java/charitan_go/charitan_api_gateway/internal/filter/JwtAuthFilter.java
@@ -8,25 +8,55 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.PathContainer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.pattern.PathPattern;
+import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @Component
 class JwtAuthFilter implements GlobalFilter, Ordered {
 
     private final JwtService jwtService;
 
+    private final List<PathPattern> publicPaths;
+
     @Autowired
     JwtAuthFilter(JwtService jwtService) {
         this.jwtService = jwtService;
+
+        PathPatternParser parser = new PathPatternParser();
+        this.publicPaths = List.of(
+                // AUTH
+                parser.parse("/api/auth/login"),
+                parser.parse("/api/auth/register")
+        );
+    }
+
+
+    private boolean isPublicPath(String path) {
+        PathContainer pathContainer = PathContainer.parsePath(path);
+        return publicPaths.stream().anyMatch(pattern -> pattern.matches(pathContainer));
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        // Get path
+        String path = exchange.getRequest().getURI().getPath();
+
+        // If is public path
+        if (isPublicPath(path)) {
+            return chain.filter(exchange);
+        }
+
+        // Get token
         String token = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
 
+        // For protected path, if token is not available => UNAUTHORIZED
         if (token == null || !token.startsWith("Bearer ")) {
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
